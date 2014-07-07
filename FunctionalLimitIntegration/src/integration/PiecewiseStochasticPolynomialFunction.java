f7a5c341dad3ee6642baf7c3984f5dc18d11d5bd
Xiaoxi Wang <vshallc@gmail.com>
2014-07-08 00:50:47 +0800

    add PiecewiseStochasticPolynomialFunction class

---
+++ FunctionalLimitIntegration/src/integration/PiecewiseStochasticPolynomialFunction.java
@@ -0,0 +1,34 @@
+package integration;
+
+import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
+import org.apache.commons.math3.exception.DimensionMismatchException;
+
+/**
+ * Created by XiaoxiWang on 7/5/14.
+ */
+public class PiecewiseStochasticPolynomialFunction {
+    StochasticPolynomialFunction[] stocPolyFuncs;
+    double[] bounds;
+
+    public PiecewiseStochasticPolynomialFunction(StochasticPolynomialFunction[] stocPolyFuncs, double[] bounds) {
+        if (stocPolyFuncs.length != bounds.length) {
+            throw new DimensionMismatchException(bounds.length, stocPolyFuncs.length);
+        }
+        if (bounds.length > 1) {
+            for (int i = 1; i < bounds.length; i++) {
+                if (bounds[i] < bounds[i - 1]) throw new IllegalArgumentException();
+            }
+        }
+        this.stocPolyFuncs = stocPolyFuncs;
+        this.bounds = bounds;
+    }
+
+    public StochasticPolynomialFunction getStochasticPolynomialFunction(
+            int piece) {
+        return stocPolyFuncs[piece];
+    }
+
+    public double[] getBounds() {
+        return this.bounds;
+    }
+}
--- FunctionalLimitIntegration/src/integration/StochasticPolynomialFunction.java
+++ FunctionalLimitIntegration/src/integration/StochasticPolynomialFunction.java
@@ -2,25 +2,41 @@ package integration;
 
 import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 
-public class StochasticPolynomialFunction extends PolynomialFunction{
-	private static final long serialVersionUID = -4912879085296725656L;
-	double sc; // coefficients for the stochastic parameter xi, xi ~ U[-1,1]
-	StochasticPolynomialFunction(double[] c, double sc){
-		super(c);
-		this.sc = sc;
-	}
-	
-	public PolynomialFunction getDeterminedPart() {
-		return (PolynomialFunction)this;
-	}
-	
-	public double getXiCoefficient() {
-		return sc;
-	}
-	
-	public StochasticPolynomialFunction add(final StochasticPolynomialFunction sp) {
-		PolynomialFunction p = this.getDeterminedPart().add(sp.getDeterminedPart());
-		return new StochasticPolynomialFunction(p.getCoefficients(), this.sc + sp.getXiCoefficient());
-	}
-	
+import java.util.Random;
+
+public class StochasticPolynomialFunction{
+    PolynomialFunction polyfun;
+    double sc; // coefficients for the stochastic parameter xi, xi ~ U[0,1]
+    Random random = new Random();
+    StochasticPolynomialFunction(double[] c, double sc) {
+        polyfun = new PolynomialFunction(c);
+        this.sc = sc;
+    }
+
+    public PolynomialFunction getDeterminedPart() {
+        return polyfun;
+    }
+
+    public double getXiCoefficient() {
+        return sc;
+    }
+
+    public StochasticPolynomialFunction add(final StochasticPolynomialFunction spf) {
+        PolynomialFunction p = polyfun.add(spf.getDeterminedPart());
+        return new StochasticPolynomialFunction(p.getCoefficients(), this.sc + spf.getXiCoefficient());
+    }
+
+    public StochasticPolynomialFunction subtract(final StochasticPolynomialFunction spf) {
+        PolynomialFunction p = polyfun.subtract(spf.getDeterminedPart());
+        return new StochasticPolynomialFunction(p.getCoefficients(), this.sc - spf.getXiCoefficient());
+    }
+
+    public double randomValue(double x) {
+        return polyfun.value(x) + sc * this.getRandomXi();
+    }
+
+    private double getRandomXi() {
+        return random.nextDouble();
+    }
+
 }
