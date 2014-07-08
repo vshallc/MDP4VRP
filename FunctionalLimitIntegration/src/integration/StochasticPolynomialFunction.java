2b687eb9ffe5583e3b5a49d3cb610e483d30f613
Xiaoxi Wang <vshallc@gmail.com>
2014-07-09 01:02:00 +0800

    using polynomial functions as coefficients of stochastic functions instead of a double.

--- FunctionalLimitIntegration/src/integration/StochasticPolynomialFunction.java
+++ FunctionalLimitIntegration/src/integration/StochasticPolynomialFunction.java
@@ -5,21 +5,21 @@ import org.apache.commons.math3.analysis.polynomials.PolynomialFunction;
 import java.util.Random;
 
 public class StochasticPolynomialFunction{
-    PolynomialFunction polyfun;
-    double sc; // coefficients for the stochastic parameter xi, xi ~ U[0,1]
+    PolynomialFunction[] polyfunCoef;   // polynomial functions as the coefficients
+                                        // for the stochastic parameter xi, xi ~ U[0,1]
+//    double sc; // coefficients for the stochastic parameter xi, xi ~ U[0,1]
     Random random = new Random();
-    StochasticPolynomialFunction(double[] c, double sc) {
-        polyfun = new PolynomialFunction(c);
-        this.sc = sc;
+    StochasticPolynomialFunction(PolynomialFunction[] polyfunCoef) {
+        this.polyfunCoef = polyfunCoef;
     }
 
     public PolynomialFunction getDeterminedPart() {
-        return polyfun;
+        return polyfunCoef[0];
     }
 
-    public double getXiCoefficient() {
-        return sc;
-    }
+//    public double getXiCoefficient() {
+//        return sc;
+//    }
 
     public StochasticPolynomialFunction add(final StochasticPolynomialFunction spf) {
         PolynomialFunction p = polyfun.add(spf.getDeterminedPart());
