a9e47e0c43242098866699f6560e0b87da520f8c
Xiaoxi Wang <vshallc@gmail.com>
2014-08-08 01:11:37 +0800

    Start developing integration part

--- FunctionalLimitIntegration/src/integration/PiecewisePolynomialFunction.java
+++ FunctionalLimitIntegration/src/integration/PiecewisePolynomialFunction.java
@@ -21,17 +21,32 @@ public class PiecewisePolynomialFunction {
         return this.bounds;
     }
 
+    public int getPieceNum() {
+        return this.polyFuncs.length;
+    }
+
     public PiecewiseStochasticPolynomialFunction compose(PiecewiseStochasticPolynomialFunction pwStocPolyFunc) {
         return null;
     }
 
-    public PiecewisePolynomialFunction integrationOnXiOfComposition(PiecewisePolynomialFunction V, PiecewiseStochasticPolynomialFunction g) {
-        // for calculating: int f(x)*V(t+g(t)) dx; x:0~1; g>0 -> t+g(t)>t
-        double[][] g_ext = g.getDeterminedExtremaOnEachPieceFiniteDomain();
-        double[][] g_xi_rng = g.getStochasticRangeOnEachPieceFiniteDomain();
-        for (int i = 0; i < g.getPieceNum(); ++i) {
-            double min = g_ext[i][0] + g_xi_rng[i][0];
-            double max = g_ext[i][1] + g_xi_rng[i][1];
+    public PiecewisePolynomialFunction integrationOnXiOfComposition_test(PiecewisePolynomialFunction V, PiecewiseStochasticPolynomialFunction A) {
+        // only for calculating: int f(x)*V(A(t)) dx; x:0~1; A(t)=t+g(t); g>0 -> t+g(t)>t
+        // A(t) arrive time (start on t)
+        // A'(t)>=0
+        double[][] g_ext = A.getDeterminedExtremaOnEachPieceFiniteDomain();
+        double[][] g_xi_range = A.getStochasticRangeOnEachPieceFiniteDomain();
+        int pieceNum = V.getPieceNum();
+        int leftBoundID = 1;
+        for (int i = 0; i < A.getPieceNum(); ++i) {
+            double min = g_ext[i][0] + g_xi_range[i][0];
+            double max = g_ext[i][1] + g_xi_range[i][1];
+            for (int j = leftBoundID; j < bounds.length; ++j) {
+                if (min >= bounds[j]) {
+                    --pieceNum;
+                } else {
+                    //
+                }
+            }
         }
         return null;
     }
