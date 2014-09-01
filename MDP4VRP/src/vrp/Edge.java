19473a6554f7c1a5df2d51240bd1a3446898f51a
Xiaoxi Wang <vshallc@gmail.com>
2014-09-02 07:51:32 +0800

    start coding vrp package

---
+++ MDP4VRP/src/vrp/Edge.java
@@ -0,0 +1,13 @@
+package vrp;
+
+/**
+ * Created by Xiaoxi Wang on 9/1/14.
+ */
+public class Edge {
+    Node startNode, endNode;
+
+    public Edge(Node startNode, Node endNode) {
+        this.startNode = startNode;
+        this.endNode = endNode;
+    }
+}
---
+++ MDP4VRP/src/vrp/Node.java
@@ -0,0 +1,9 @@
+package vrp;
+
+/**
+ * Created by Xiaoxi Wang on 9/1/14.
+ */
+public class Node {
+    Edge[] incomingEdges, outgoingEdges;
+
+}
